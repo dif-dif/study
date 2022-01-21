77ed2ecb8552807df08bc8c51456fad03e197030
Alla Makhotka <all.mahotka2012@yandex.ru>
2022-01-21 17:51:01 +0300

    the day of exam

---
+++ algorithms/quick_sort.py
@@ -0,0 +1,14 @@
+
+def quicksort(nums):
+   if len(nums) <= 1:
+       return nums
+   else:
+        q = nums[len(nums) // 2]
+        l_nums = [n for n in nums if n < q]
+
+        e_nums = [q] * nums.count(q)
+        b_nums = [n for n in nums if n > q]
+        return quicksort(l_nums) + e_nums + quicksort(b_nums)
+
+a = [1, 6, 9, 2, 5, 4]
+print(quicksort(a))
